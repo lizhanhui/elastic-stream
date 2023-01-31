6ffe48afceb151c24192534ea31caa0d86632599
Ning Yu <chillax65536@gmail.com>
2023-01-31 21:09:22 +0800

    WIP: test(etcdutl)
    Signed-off-by: Ning Yu <chillax65536@gmail.com>

--- go.mod
+++ go.mod
@@ -3,19 +3,22 @@ module github.com/AutoMQ/placement-manager
 go 1.19
 
 require (
+	github.com/cakturk/go-netstat v0.0.0-20200220111822-e5b49efee7a5
 	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.8.1
 	go.etcd.io/etcd/client/v3 v3.5.7
 	go.etcd.io/etcd/server/v3 v3.5.7
+	go.uber.org/goleak v1.2.0
 	go.uber.org/zap v1.24.0
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cakturk/go-netstat v0.0.0-20200220111822-e5b49efee7a5 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/coreos/go-semver v0.3.1 // indirect
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -33,6 +36,7 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.14.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.39.0 // indirect
@@ -69,5 +73,6 @@ require (
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- pkg/util/etcdutil/kv_test.go
+++ pkg/util/etcdutil/kv_test.go
@@ -15,8 +15,12 @@
 package etcdutil
 
 import (
+	"context"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/server/v3/embed"
 	"go.uber.org/goleak"
 
 	"github.com/AutoMQ/placement-manager/pkg/util/testutil"
@@ -28,8 +32,78 @@ func TestMain(m *testing.M) {
 
 func TestGetValue(t *testing.T) {
 	t.Parallel()
-	// re := require.New(t)
+	re := require.New(t)
+	etcd, client := prepare(re, t)
 
-	config := testutil.NewEtcdConfig(t)
-	t.Log(config)
+	tests := []struct {
+		inKeys        []string
+		inValues      []string
+		key           string
+		opts          []clientv3.OpOption
+		outKeys       []string
+		outValues     []string
+		expectedError bool
+	}{
+		{
+			inKeys:        []string{"key1"},
+			inValues:      []string{"val1"},
+			key:           "key1",
+			opts:          []clientv3.OpOption{},
+			outKeys:       []string{"key1"},
+			outValues:     []string{"val1"},
+			expectedError: false,
+		},
+	}
+	t.Log("start test")
+
+	for _, rt := range tests {
+		t.Run("name", func(t *testing.T) {
+			// prepare
+			kv := clientv3.NewKV(client)
+			for i := range rt.inKeys {
+				t.Log(i)
+				_, err := kv.Put(context.TODO(), rt.inKeys[i], rt.inValues[i])
+				re.NoError(err)
+			}
+
+			// run
+			resp, err := GetValue(client, rt.key, rt.opts...)
+
+			// check
+			if rt.expectedError {
+				re.Error(err)
+			} else {
+				re.NoError(err)
+				for i := range resp.Kvs {
+					re.Equal(rt.outKeys[i], string(resp.Kvs[i].Key))
+					re.Equal(rt.outValues[i], string(resp.Kvs[i].Value))
+				}
+			}
+		})
+	}
+
+	etcd.Close()
+
+}
+
+func prepare(re *require.Assertions, t *testing.T) (*embed.Etcd, *clientv3.Client) {
+	// start etcd
+	cfg := testutil.NewEtcdConfig(t)
+	etcd, err := embed.StartEtcd(cfg)
+	defer func() {
+		etcd.Close()
+	}()
+	re.NoError(err)
+
+	// new client
+	ep := cfg.LCUrls[0].String()
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{ep},
+	})
+	re.NoError(err)
+
+	<-etcd.Server.ReadyNotify()
+	t.Log("ready")
+
+	return etcd, client
 }
