83aa8a26e923550626fd778f2215046ce518a599
Li Zhanhui <lizhanhui@gmail.com>
2023-02-10 05:37:06 +0000

    Use scripts to install deps if missing
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

---
+++ scripts/install_deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+try_install_flatc() {
+    if [ ! -f /usr/local/bin/flatc ]; then
+        sudo apt-get update
+        sudo apt-get install -y unzip
+        wget -O flatc.zip https://github.com/google/flatbuffers/releases/download/v23.1.4/Linux.flatc.binary.g++-10.zip
+        unzip flatc.zip
+        sudo mv flatc /usr/local/bin/
+        rm flatc.zip
+    else
+        echo "flatc exists"
+    fi
+}
+
+try_install_flatc
