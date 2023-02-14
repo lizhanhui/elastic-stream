4c6d59faf74cc9121d3663a9a1f526e60da7098c
daniel-y <daniel@automq.com>
2023-02-14 20:20:27 +0800

    Asign ids for all the fileds in flatbuffer tables

--- components/protocol/build.rs
+++ components/protocol/build.rs
@@ -2,7 +2,7 @@ use flatc_rust;
 use std::path::Path;
 
 fn main() {
-    println!("cargo:rerun-if-changed=fbs/rpc.fbs");
+    println!("cargo:rerun-if-changed=fbs/");
     flatc_rust::run(flatc_rust::Args {
         inputs: &[Path::new("fbs/rpc.fbs"), Path::new("fbs/model.fbs")],
         out_dir: Path::new("src/generated/"),
