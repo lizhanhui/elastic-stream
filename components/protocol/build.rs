8cae0306aedb7b4f46a19e1fe6c99e12a3309642
daniel-y <daniel@automq.com>
2023-02-14 20:09:42 +0800

    Add fbs for record model

--- components/protocol/build.rs
+++ components/protocol/build.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 fn main() {
     println!("cargo:rerun-if-changed=fbs/rpc.fbs");
     flatc_rust::run(flatc_rust::Args {
-        inputs: &[Path::new("fbs/rpc.fbs")],
+        inputs: &[Path::new("fbs/rpc.fbs"), Path::new("fbs/model.fbs")],
         out_dir: Path::new("src/generated/"),
         ..Default::default()
     })
