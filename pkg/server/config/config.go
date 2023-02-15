c0c93db707f55987328a8ded9330e2036d9f0618
Ning Yu <chillax65536@gmail.com>
2023-02-15 14:11:16 +0800

    feature(config)" Configure flags
    Signed-off-by: Ning Yu <chillax65536@gmail.com>

--- pkg/server/config/config.go
+++ pkg/server/config/config.go
@@ -23,6 +23,7 @@ import (
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"go.etcd.io/etcd/server/v3/embed"
+	"go.uber.org/zap"
 
 	"github.com/AutoMQ/placement-manager/pkg/util/typeutil"
 )
@@ -60,6 +61,8 @@ type Config struct {
 	LeaderLease int64
 
 	LeaderPriorityCheckInterval typeutil.Duration
+
+	lg *zap.Logger
 }
 
 // NewConfig creates a new config.
@@ -67,65 +70,52 @@ func NewConfig(arguments []string) (*Config, error) {
 	cfg := &Config{}
 	cfg.etcd = embed.NewConfig()
 
-	v := viper.New()
-	fs := pflag.NewFlagSet("PD", pflag.ContinueOnError)
+	v, fs := configure()
 
-	// Viper settings
-	v.AddConfigPath(".")
-	v.AddConfigPath("$CONFIG_DIR/")
-
-	fs.String("peer-urls", _defaultPeerUrls, "urls for peer traffic")
-	fs.String("client-urls", _defaultClientUrls, "urls for client traffic")
-	fs.String("advertise-peer-urls", "", "advertise urls for peer traffic (default '${peer-urls}')")
-	fs.String("advertise-client-urls", "", "advertise urls for client traffic (default '${client-urls}')")
-	_ = v.BindPFlag("peer-urls", fs.Lookup("peer-urls"))
-	_ = v.BindPFlag("client-urls", fs.Lookup("client-urls"))
-	_ = v.BindPFlag("advertise-peer-urls", fs.Lookup("advertise-peer-urls"))
-	_ = v.BindPFlag("advertise-client-urls", fs.Lookup("advertise-client-urls"))
-	v.RegisterAlias("PeerUrls", "peer-urls")
-	v.RegisterAlias("ClientUrls", "client-urls")
-	v.RegisterAlias("AdvertisePeerUrls", "advertise-peer-urls")
-	v.RegisterAlias("AdvertiseClientUrls", "advertise-client-urls")
-
-	fs.String("name", "", "human-readable name for this PM member (default 'PM-${hostname}')")
-	fs.String("data-dir", "", "path to the data directory (default 'default.${name}')")
-	_ = v.BindPFlag("name", fs.Lookup("name"))
-	_ = v.BindPFlag("data-dir", fs.Lookup("data-dir"))
-	v.RegisterAlias("DataDir", "data-dir")
-	fs.String("initial-cluster", "", "initial cluster configuration for bootstrapping, e.g. pd=http://127.0.0.1:2380. (default 'pd=${advertise-peer-urls}')")
-	_ = v.BindPFlag("initial-cluster", fs.Lookup("initial-cluster"))
-	v.RegisterAlias("InitialCluster", "initial-cluster")
-	v.SetDefault("etcd.initialClusterToken", _defaultInitialClusterToken)
-	v.SetDefault("etcd.enableGRPCGateway", _defaultEnableGRPCGateway)
-
-	fs.Int64("leader-lease", _defaultLeaderLease, "expiration time of the leader, in seconds")
-	_ = v.BindPFlag("leader-lease", fs.Lookup("leader-lease"))
-	v.RegisterAlias("LeaderLease", "lease-lease")
-
-	fs.Duration("leader-priority-check-interval", _defaultLeaderPriorityCheckInterval, "time interval for checking the leader's priority")
-	_ = v.BindPFlag("leader-priority-check-interval", fs.Lookup("leader-priority-check-interval"))
-	v.RegisterAlias("LeaderPriorityCheckInterval", "leader-priority-check-interval")
-
-	// TODO
-	//err := v.ReadInConfig()
-	//if err != nil {
-	//	if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-	//		return nil, err
-	//	}
-	//}
-
-	//err = fs.Parse(arguments)
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	err := v.Unmarshal(cfg)
+	// parse from command line
+	fs.String("config", "", "configuration file")
+	err := fs.Parse(arguments)
 	if err != nil {
 		return nil, err
 	}
 
+	// read configuration from file
+	c, _ := fs.GetString("config")
+	v.SetConfigFile(c)
+	err = v.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, errors.Wrap(err, "read configuration file")
+		}
+	}
+
+	// set config
+	err = v.Unmarshal(cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "unmarshal configuration")
+	}
+
+	// TODO new and set logger
+	logger := cfg.lg
+
+	if configFile := v.ConfigFileUsed(); configFile != "" {
+		logger.Info("load configuration from file.", zap.String("file-name", configFile))
+	}
+
 	cfg.v = v
 	return cfg, nil
+}
+
+// Adjust generates default values for some fields (if they are empty)
+func (c *Config) Adjust() error {
+	// TODO
+	return nil
+}
+
+// Validate checks whether the configuration is valid. It should be called after Adjust
+func (c *Config) Validate() error {
+	// TODO
+	return nil
 }
 
 // Etcd generates a configuration for embedded etcd.
@@ -154,7 +144,55 @@ func (c *Config) Etcd() (*embed.Config, error) {
 		return nil, errors.Wrap(err, "parse advertise client url")
 	}
 
-	return &cfg, nil
+	return cfg, nil
+}
+
+// Logger returns logger generated based on the config
+func (c *Config) Logger() *zap.Logger {
+	return c.lg
+}
+
+func configure() (*viper.Viper, *pflag.FlagSet) {
+	v := viper.New()
+	fs := pflag.NewFlagSet("PD", pflag.ContinueOnError)
+
+	// Viper settings
+	v.AddConfigPath(".")
+	v.AddConfigPath("$CONFIG_DIR/")
+
+	// etcd urls settings
+	fs.String("peer-urls", _defaultPeerUrls, "urls for peer traffic")
+	fs.String("client-urls", _defaultClientUrls, "urls for client traffic")
+	fs.String("advertise-peer-urls", "", "advertise urls for peer traffic (default '${peer-urls}')")
+	fs.String("advertise-client-urls", "", "advertise urls for client traffic (default '${client-urls}')")
+	_ = v.BindPFlag("peer-urls", fs.Lookup("peer-urls"))
+	_ = v.BindPFlag("client-urls", fs.Lookup("client-urls"))
+	_ = v.BindPFlag("advertise-peer-urls", fs.Lookup("advertise-peer-urls"))
+	_ = v.BindPFlag("advertise-client-urls", fs.Lookup("advertise-client-urls"))
+	v.RegisterAlias("PeerUrls", "peer-urls")
+	v.RegisterAlias("ClientUrls", "client-urls")
+	v.RegisterAlias("AdvertisePeerUrls", "advertise-peer-urls")
+	v.RegisterAlias("AdvertiseClientUrls", "advertise-client-urls")
+
+	// PM members settings
+	fs.String("name", "", "human-readable name for this PM member (default 'PM-${hostname}')")
+	fs.String("data-dir", "", "path to the data directory (default 'default.${name}')")
+	fs.String("initial-cluster", "", "initial cluster configuration for bootstrapping, e.g. pd=http://127.0.0.1:2380. (default 'pd=${advertise-peer-urls}')")
+	fs.Int64("leader-lease", _defaultLeaderLease, "expiration time of the leader, in seconds")
+	fs.Duration("leader-priority-check-interval", _defaultLeaderPriorityCheckInterval, "time interval for checking the leader's priority")
+	_ = v.BindPFlag("name", fs.Lookup("name"))
+	_ = v.BindPFlag("data-dir", fs.Lookup("data-dir"))
+	_ = v.BindPFlag("initial-cluster", fs.Lookup("initial-cluster"))
+	_ = v.BindPFlag("leader-lease", fs.Lookup("leader-lease"))
+	_ = v.BindPFlag("leader-priority-check-interval", fs.Lookup("leader-priority-check-interval"))
+	v.RegisterAlias("DataDir", "data-dir")
+	v.RegisterAlias("InitialCluster", "initial-cluster")
+	v.RegisterAlias("LeaderLease", "lease-lease")
+	v.RegisterAlias("LeaderPriorityCheckInterval", "leader-priority-check-interval")
+	v.SetDefault("etcd.initialClusterToken", _defaultInitialClusterToken)
+	v.SetDefault("etcd.enableGRPCGateway", _defaultEnableGRPCGateway)
+
+	return v, fs
 }
 
 // parseUrls parse a string into multiple urls.
