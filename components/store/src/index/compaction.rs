76f967cebceed6e1e6f7a57ab7831fd5b3c8ee7a
Li Zhanhui <lizhanhui@gmail.com>
2023-03-04 00:08:11 +0800

    Set up RocksDB without option tuning
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

---
+++ components/store/src/index/compaction.rs
@@ -0,0 +1,74 @@
+use std::{
+    error::Error,
+    ffi::{CStr, CString},
+    sync::atomic::{AtomicUsize, Ordering},
+};
+
+use rocksdb::{
+    compaction_filter::CompactionFilter,
+    compaction_filter_factory::{CompactionFilterContext, CompactionFilterFactory},
+    CompactionDecision,
+};
+
+pub(crate) struct IndexCompactionFilter {
+    name: CString,
+}
+
+impl IndexCompactionFilter {
+    pub(crate) fn new(n: usize) -> Result<Self, Box<dyn Error>> {
+        let name = format!("round-{}", n);
+        let name = CString::new(name)?;
+        Ok(Self { name })
+    }
+}
+
+impl CompactionFilter for IndexCompactionFilter {
+    fn filter(&mut self, level: u32, key: &[u8], value: &[u8]) -> CompactionDecision {
+        println!(
+            "Level: {} Checking {} --> {}",
+            level,
+            String::from_utf8_lossy(key),
+            String::from_utf8_lossy(value)
+        );
+        CompactionDecision::Keep
+    }
+
+    fn name(&self) -> &CStr {
+        &self.name
+    }
+}
+
+pub(crate) struct IndexCompactionFilterFactory {
+    name: CString,
+    round: AtomicUsize,
+}
+
+impl IndexCompactionFilterFactory {
+    pub(crate) fn new(name: &str) -> Result<Self, Box<dyn Error>> {
+        let name = CString::new(name)?;
+        Ok(Self {
+            name,
+            round: AtomicUsize::new(0),
+        })
+    }
+}
+
+impl CompactionFilterFactory for IndexCompactionFilterFactory {
+    type Filter = IndexCompactionFilter;
+
+    fn create(&mut self, context: CompactionFilterContext) -> Self::Filter {
+        if context.is_full_compaction {
+            println!("Full Compaction");
+        }
+
+        if context.is_manual_compaction {
+            println!("Manual Compaction");
+        }
+
+        IndexCompactionFilter::new(self.round.fetch_add(1, Ordering::Relaxed)).unwrap()
+    }
+
+    fn name(&self) -> &CStr {
+        &self.name
+    }
+}
--- components/store/src/index/mod.rs
+++ components/store/src/index/mod.rs
@@ -1,3 +1,5 @@
+mod compaction;
+
 pub(crate) struct Indexer {}
 
 impl Indexer {
@@ -14,4 +16,67 @@ impl Indexer {
 
     /// Flush record index in cache into RocksDB using atomic-flush.
     pub(crate) fn flush(&self) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{error::Error, path::Path};
+
+    use rocksdb::{ColumnFamilyDescriptor, Options, DB};
+
+    use super::compaction;
+
+    #[test]
+    fn test_rocksdb_setup() -> Result<(), Box<dyn Error>> {
+        let path = "/tmp/rocksdb";
+        let path = Path::new(path);
+        std::fs::create_dir_all(path)?;
+        let _dir_guard = util::DirectoryRemovalGuard::new(&path);
+
+        let mut cf_opts = Options::default();
+        cf_opts.set_max_write_buffer_number(16);
+
+        let index_compaction_filter_factory =
+            compaction::IndexCompactionFilterFactory::new("index-compaction-filter")?;
+
+        cf_opts.set_compaction_filter_factory(index_compaction_filter_factory);
+
+        let cf = ColumnFamilyDescriptor::new("index", cf_opts);
+
+        let mut db_opts = Options::default();
+        db_opts.create_if_missing(true);
+        db_opts.create_missing_column_families(true);
+
+        {
+            let db = DB::open_cf_descriptors(&db_opts, path, vec![cf])?;
+
+            let cf = db.cf_handle("index").unwrap();
+
+            db.put_cf(cf, "abc", "def")?;
+
+            match db.get_cf(cf, "abc") {
+                Ok(Some(v)) => {
+                    let value = String::from_utf8_lossy(&v[..]);
+                    println!("value = {}", value);
+                }
+                Ok(None) => {}
+                Err(e) => {}
+            }
+
+            (0..100)
+                .into_iter()
+                .map(|n| {
+                    let key = format!("k{:08}", n);
+                    let value = format!("v{:08}", n);
+                    db.put_cf(cf, key, value)?;
+                    Ok::<(), rocksdb::Error>(())
+                })
+                .flatten()
+                .count();
+
+            db.compact_range_cf(cf, None::<&[u8]>, None::<&[u8]>);
+        }
+
+        Ok(())
+    }
 }
