8ff8a97e580f8f3f7ae021958353da9d2bd0b5d7
Li Zhanhui <lizhanhui@gmail.com>
2023-03-05 22:23:34 +0800

    Install clang

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,7 +3,7 @@
 try_install_flatc() {
     if [ ! -f /usr/local/bin/flatc ]; then
         sudo apt-get update
-        sudo apt-get install -y unzip
+        sudo apt-get install -y unzip clang
         wget -O flatc.zip https://github.com/google/flatbuffers/releases/download/v23.1.4/Linux.flatc.binary.g++-10.zip
         unzip flatc.zip
         sudo mv flatc /usr/local/bin/
