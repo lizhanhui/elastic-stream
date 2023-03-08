7a38822fabf70860450ea02d80cfb9c19e6798d3
Ning Yu <chillax65536@gmail.com>
2023-03-08 22:21:51 +0800

    perf(sbp/protocol): Support more formats in `Request` and `Response`
    Signed-off-by: Ning Yu <chillax65536@gmail.com>

--- pkg/sbp/protocol/request.go
+++ pkg/sbp/protocol/request.go
@@ -7,7 +7,9 @@ import (
 	"github.com/AutoMQ/placement-manager/pkg/sbp/codec/format"
 )
 
-var errUnsupported = errors.New("unsupported format")
+const (
+	_unsupportedReqErrMsg = "unsupported request format: %s"
+)
 
 // Request is an SBP request
 type Request interface {
@@ -16,18 +18,64 @@ type Request interface {
 	Unmarshal(fmt format.Format, data []byte) error
 }
 
+type unmarshaler interface {
+	flatBufferUnmarshaler
+	protoBufferUnmarshaler
+	jsonUnmarshaler
+}
+
+type flatBufferUnmarshaler interface {
+	unmarshalFlatBuffer(data []byte) error
+}
+
+type protoBufferUnmarshaler interface {
+	unmarshalProtoBuffer(data []byte) error
+}
+
+type jsonUnmarshaler interface {
+	unmarshalJSON(data []byte) error
+}
+
+// baseRequest is a default implementation of unmarshaler.
+type baseRequest struct{}
+
+func (b *baseRequest) unmarshalFlatBuffer(_ []byte) error {
+	return errors.Errorf(_unsupportedReqErrMsg, format.FlatBuffer())
+}
+
+func (b *baseRequest) unmarshalProtoBuffer(_ []byte) error {
+	return errors.Errorf(_unsupportedReqErrMsg, format.ProtoBuffer())
+}
+
+func (b *baseRequest) unmarshalJSON(_ []byte) error {
+	return errors.Errorf(_unsupportedReqErrMsg, format.JSON())
+}
+
 // ListRangesRequest is a request to operation.ListRange
 type ListRangesRequest struct {
+	baseRequest
 	*rpcfb.ListRangesRequestT
+}
+
+func (l *ListRangesRequest) unmarshalFlatBuffer(data []byte) error {
+	l.ListRangesRequestT = rpcfb.GetRootAsListRangesRequest(data, 0).UnPack()
+	return nil
 }
 
 //nolint:revive // EXC0012 comment already exists in interface
 func (l *ListRangesRequest) Unmarshal(fmt format.Format, data []byte) error {
+	return unmarshal(l, fmt, data)
+}
+
+func unmarshal(request unmarshaler, fmt format.Format, data []byte) error {
 	switch fmt {
 	case format.FlatBuffer():
-		rpcfb.GetRootAsListRangesRequest(data, 0).UnPackTo(l.ListRangesRequestT)
-		return nil
+		return request.unmarshalFlatBuffer(data)
+	case format.ProtoBuffer():
+		return request.unmarshalProtoBuffer(data)
+	case format.JSON():
+		return request.unmarshalJSON(data)
 	default:
-		return errUnsupported
+		return errors.Errorf(_unsupportedReqErrMsg, fmt)
 	}
 }
