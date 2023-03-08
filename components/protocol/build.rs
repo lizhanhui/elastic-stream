4ac1c7d55e14ae9078ff1a5388e9e1d2566e6076
Li Zhanhui <lizhanhui@gmail.com>
2023-03-08 16:33:26 +0800

    Create out_dir for generated source files
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- components/protocol/build.rs
+++ components/protocol/build.rs
@@ -3,18 +3,27 @@ use std::path::Path;
 
 fn main() {
     println!("cargo:rerun-if-changed=fbs/");
+    let out_dir = Path::new("src/generated/");
+    if !out_dir.exists() {
+        std::fs::create_dir_all(out_dir);
+    }
+
     flatc_rust::run(flatc_rust::Args {
         inputs: &[Path::new("fbs/rpc.fbs"), Path::new("fbs/model.fbs")],
-        out_dir: Path::new("src/generated/"),
+        out_dir,
         ..Default::default()
     })
     .expect("flatc");
 
     println!("cargo:rerun-if-changed=fbs/");
+    let out_dir = Path::new("../../sdks/java/lib/src/generated/java");
+    if !out_dir.exists() {
+        std::fs::create_dir_all(out_dir);
+    }
     flatc_rust::run(flatc_rust::Args {
         lang: "java",
         inputs: &[Path::new("fbs/rpc.fbs"), Path::new("fbs/model.fbs")],
-        out_dir: Path::new("../../sdks/java/lib/src/generated/java"),
+        out_dir,
         ..Default::default()
     })
     .expect("flatc");
