b7cf27da78cf81507d0cdca271dcde85d32d5535
Ning Yu <chillax65536@gmail.com>
2023-03-13 17:20:43 +0800

    feature(cluster/cache): Use Concurrent Map
    Signed-off-by: Ning Yu <chillax65536@gmail.com>

--- go.mod
+++ go.mod
@@ -26,7 +26,6 @@ require (
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/brianvoe/gofakeit/v6 v6.20.2 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/coreos/go-semver v0.3.1 // indirect
@@ -51,6 +50,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/orcaman/concurrent-map/v2 v2.0.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.14.0 // indirect
--- pkg/server/cluster/cache/cache.go
+++ pkg/server/cluster/cache/cache.go
@@ -1,44 +1,48 @@
 package cache
 
 import (
+	cmap "github.com/orcaman/concurrent-map/v2"
+
 	"github.com/AutoMQ/placement-manager/api/rpcfb/rpcfb"
 )
 
 // Stream is the cache for StreamT and its ranges.
 type Stream struct {
 	*rpcfb.StreamT
-	ranges map[int32]*rpcfb.RangeT
+	ranges cmap.ConcurrentMap[int32, *rpcfb.RangeT]
+}
+
+func NewStream(stream *rpcfb.StreamT) *Stream {
+	return &Stream{
+		StreamT: stream,
+		ranges:  cmap.NewWithCustomShardingFunction[int32, *rpcfb.RangeT](func(key int32) uint32 { return uint32(key) }),
+	}
 }
 
 // Cache is the cache for all metadata.
 type Cache struct {
-	streams map[int64]*Stream
+	streams cmap.ConcurrentMap[int64, *Stream]
 	// TODO: add more cache
 }
 
 // NewCache creates a new Cache.
 func NewCache() *Cache {
 	return &Cache{
-		// TODO init other fields
+		streams: cmap.NewWithCustomShardingFunction[int64, *Stream](func(key int64) uint32 { return uint32(key) }),
 	}
 }
 
 // Reset resets the cache.
 func (c *Cache) Reset() {
-	c.streams = make(map[int64]*Stream)
+	c.streams.Clear()
 }
 
 // SaveStream saves a stream to the cache.
 func (c *Cache) SaveStream(stream *rpcfb.StreamT) {
-	// TODO lock it
-	c.streams[stream.StreamId] = &Stream{
-		StreamT: stream,
-		ranges:  make(map[int32]*rpcfb.RangeT),
-	}
+	c.streams.Set(stream.StreamId, NewStream(stream))
 }
 
 // StreamCount returns the count of streams in the cache.
 func (c *Cache) StreamCount() int {
-	// TODO lock it
-	return len(c.streams)
+	return c.streams.Count()
 }
