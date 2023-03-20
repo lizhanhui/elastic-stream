3cb1e685db4205abf083d7d7925f3a38145d08fc
Li Zhanhui <lizhanhui@gmail.com>
2023-03-20 10:01:47 +0800

    Add crate doc for placement-client
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- components/placement-client/src/lib.rs
+++ components/placement-client/src/lib.rs
@@ -1,3 +1,11 @@
+//! Data node uses placement clients to talk to placement managers.
+//!
+//! As a result, placement clients shall comply with `thread-per-core` threading model. Further,
+//! placement clients reuse the same `tokio-uring` network library stack to initiate requests and
+//! reap completed responses.
+//!  
+//! For applications that need to talk to `PlacementManager` and `DataNode`, please use crate `front-end-sdk`.
+
 #![feature(try_find)]
 
 pub mod client;
