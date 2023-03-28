467645f456c9f0a27859f06aa039e1147c951c2d
Ning Yu <chillax65536@gmail.com>
2023-03-28 16:11:02 +0800

    feature(api/rpcfb): Sync with elastic-storage(9818b6b6c69b0393874da7f268aaaec88b4fb155)
    Signed-off-by: Ning Yu <chillax65536@gmail.com>

--- api/rpcfb/rpcfb/IdAllocationRequest.go
+++ api/rpcfb/rpcfb/IdAllocationRequest.go
@@ -7,6 +7,7 @@ import (
 )
 
 type IdAllocationRequestT struct {
+	TimeoutMs int32 `json:"timeout_ms"`
 	Host string `json:"host"`
 }
 
@@ -17,11 +18,13 @@ func (t *IdAllocationRequestT) Pack(builder *flatbuffers.Builder) flatbuffers.UO
 		hostOffset = builder.CreateString(t.Host)
 	}
 	IdAllocationRequestStart(builder)
+	IdAllocationRequestAddTimeoutMs(builder, t.TimeoutMs)
 	IdAllocationRequestAddHost(builder, hostOffset)
 	return IdAllocationRequestEnd(builder)
 }
 
 func (rcv *IdAllocationRequest) UnPackTo(t *IdAllocationRequestT) {
+	t.TimeoutMs = rcv.TimeoutMs()
 	t.Host = string(rcv.Host())
 }
 
@@ -59,8 +62,20 @@ func (rcv *IdAllocationRequest) Table() flatbuffers.Table {
 	return rcv._tab
 }
 
-func (rcv *IdAllocationRequest) Host() []byte {
+func (rcv *IdAllocationRequest) TimeoutMs() int32 {
 	o := flatbuffers.UOffsetT(rcv._tab.Offset(4))
+	if o != 0 {
+		return rcv._tab.GetInt32(o + rcv._tab.Pos)
+	}
+	return 0
+}
+
+func (rcv *IdAllocationRequest) MutateTimeoutMs(n int32) bool {
+	return rcv._tab.MutateInt32Slot(4, n)
+}
+
+func (rcv *IdAllocationRequest) Host() []byte {
+	o := flatbuffers.UOffsetT(rcv._tab.Offset(6))
 	if o != 0 {
 		return rcv._tab.ByteVector(o + rcv._tab.Pos)
 	}
@@ -68,10 +83,13 @@ func (rcv *IdAllocationRequest) Host() []byte {
 }
 
 func IdAllocationRequestStart(builder *flatbuffers.Builder) {
-	builder.StartObject(1)
+	builder.StartObject(2)
+}
+func IdAllocationRequestAddTimeoutMs(builder *flatbuffers.Builder, timeoutMs int32) {
+	builder.PrependInt32Slot(0, timeoutMs, 0)
 }
 func IdAllocationRequestAddHost(builder *flatbuffers.Builder, host flatbuffers.UOffsetT) {
-	builder.PrependUOffsetTSlot(0, flatbuffers.UOffsetT(host), 0)
+	builder.PrependUOffsetTSlot(1, flatbuffers.UOffsetT(host), 0)
 }
 func IdAllocationRequestEnd(builder *flatbuffers.Builder) flatbuffers.UOffsetT {
 	return builder.EndObject()
