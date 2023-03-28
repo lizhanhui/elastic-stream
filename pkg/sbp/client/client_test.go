5a4e1048793fef9157f4b3f0c72b816354c0742e
Ning Yu <chillax65536@gmail.com>
2023-03-28 15:14:52 +0800

    test(sbp/client): Test request timeout
    Signed-off-by: Ning Yu <chillax65536@gmail.com>

--- pkg/sbp/client/client_test.go
+++ pkg/sbp/client/client_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 	"go.uber.org/zap"
@@ -31,22 +32,44 @@ func TestClient_SealRanges(t *testing.T) {
 	client := NewClient(logger)
 	defer client.CloseIdleConnections()
 
-	req := &protocol.SealRangesRequest{
-		SealRangesRequestT: rpcfb.SealRangesRequestT{
-			Ranges: []*rpcfb.RangeIdT{
-				{
-					StreamId:   1,
-					RangeIndex: 2,
-				},
+	req := &protocol.SealRangesRequest{SealRangesRequestT: rpcfb.SealRangesRequestT{
+		Ranges: []*rpcfb.RangeIdT{
+			{
+				StreamId:   1,
+				RangeIndex: 2,
 			},
 		},
-	}
+	}}
 	resp, err := client.SealRanges(req, addr)
 	re.NoError(err)
 	re.Equal(rpcfb.ErrorCodeOK, resp.Status.Code)
 	re.Len(resp.SealResponses, 1)
 	re.Equal(rpcfb.ErrorCodeOK, resp.SealResponses[0].Status.Code)
 	re.Equal(int32(2), resp.SealResponses[0].Range.RangeIndex)
+}
+
+func TestClientTimeout(t *testing.T) {
+	t.Parallel()
+	logger := zap.NewNop()
+	re := require.New(t)
+
+	addr, shutdown := startServer(t, &mockHandler{SealRangesF: func(req *protocol.SealRangesRequest, resp *protocol.SealRangesResponse) {
+		time.Sleep(40 * time.Millisecond)
+	}}, logger)
+	defer shutdown()
+
+	client := NewClient(logger)
+	defer client.CloseIdleConnections()
+
+	req := &protocol.SealRangesRequest{SealRangesRequestT: rpcfb.SealRangesRequestT{
+		TimeoutMs: 20,
+	}}
+	now := time.Now()
+	_, err := client.SealRanges(req, addr)
+	cost := time.Since(now)
+	re.True(errors.Is(err, context.DeadlineExceeded))
+	re.Greater(cost, 20*time.Millisecond)
+	re.Less(cost, 40*time.Millisecond)
 }
 
 func startServer(tb testing.TB, handler server.Handler, lg *zap.Logger) (addr string, shutdown func()) {
@@ -71,9 +94,11 @@ func startServer(tb testing.TB, handler server.Handler, lg *zap.Logger) (addr st
 
 type mockHandler struct {
 	baseHandler
+
+	SealRangesF func(req *protocol.SealRangesRequest, resp *protocol.SealRangesResponse)
 }
 
-func (m *mockHandler) SealRanges(req *protocol.SealRangesRequest, resp *protocol.SealRangesResponse) {
+func normalSealRanges(req *protocol.SealRangesRequest, resp *protocol.SealRangesResponse) {
 	results := make([]*rpcfb.SealRangesResultT, 0, len(req.Ranges))
 	for _, r := range req.Ranges {
 		results = append(results, &rpcfb.SealRangesResultT{
@@ -90,6 +115,14 @@ func (m *mockHandler) SealRanges(req *protocol.SealRangesRequest, resp *protocol
 		SealResponses: results,
 	}
 	resp.OK()
+}
+
+func (m *mockHandler) SealRanges(req *protocol.SealRangesRequest, resp *protocol.SealRangesResponse) {
+	if m.SealRangesF != nil {
+		m.SealRangesF(req, resp)
+		return
+	}
+	normalSealRanges(req, resp)
 }
 
 type baseHandler struct{}
