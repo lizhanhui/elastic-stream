02ae8860276face3a24695e52e8fe8b6626f6c13
Zhanhui Li <lizhanhui@gmail.com>
2023-05-25 19:31:11 +0800

    feat(frontend): Rename sdks subdirectories (#297)
    Rename sdks subdirectories
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- sdks/build.sh
+++ sdks/build.sh
@@ -2,6 +2,6 @@
 
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR/.." || exit
-cargo build -p frontend -Z unstable-options --out-dir=sdks/sdk-java/frontend/src/main/resources/META-INF/native/
-cd sdks/sdk-java || exit
+cargo build -p frontend -Z unstable-options --out-dir=sdks/frontend-java/frontend/src/main/resources/META-INF/native/
+cd sdks/frontend-java || exit
 mvn package
