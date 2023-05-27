0aec086bb222facc4606d1d2e8bb29bc9355bdd0
Robin Han <hanxvdovehx@gmail.com>
2023-05-27 19:19:21 +0800

    feat(frontend): add reopen stream and more append / fetch test (#340)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- sdks/frontend-rs/examples/main.rs
+++ sdks/frontend-rs/examples/main.rs
@@ -1,10 +1,9 @@
-use std::time::Duration;
-
 use bytes::{Bytes, BytesMut};
 use frontend::{Frontend, StreamOptions};
 use log::{info, LevelFilter};
 use model::{record::flat_record::FlatRecordBatch, RecordBatch};
 use std::io::Write;
+use tokio::time::{sleep, Duration};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::Builder::new()
@@ -33,7 +32,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         info!("Created stream with id: {}", stream_id);
         let stream = frontend.open(stream_id, 0).await?;
 
-        // append 10 record batch
+        info!("Step1: append 10 record batch");
         for i in 0..10 {
             let payload = Bytes::from(format!("Hello World {i:0>8}!"));
             let record_batch = RecordBatch::new_builder()
@@ -53,8 +52,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             info!("Append result: {:#?}", append_result);
         }
 
-        // fetch and check
-        // single read
+        info!("Step2: read 10 record batch one by one");
         for i in 0..10 {
             let start = i;
             let end = i + 10;
@@ -62,6 +60,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let records = decode_flat_record_batch(&mut bytes)?;
             assert_eq!(1, records.len());
             let record = records.iter().next().unwrap();
+            assert_eq!(i * 10, record.base_offset());
             assert_eq!(
                 record.payload(),
                 Bytes::from(format!("Hello World {i:0>8}!", i = i))
@@ -69,9 +68,68 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             info!("Fetch [{start}, {end}) result: {:#?}", record);
         }
 
-        // crosss read
+        info!("Step3: cross read 10 record batch");
+        for i in 0..9 {
+            let start = i + 1; // in middle of first record batch
+            let end = i + 11; // in middle of next record batch
+            let mut bytes = vec_bytes_to_bytes(stream.read(start, end, i32::MAX).await?);
+            let records = decode_flat_record_batch(&mut bytes)?;
+            // exepct read 2 record batch
+            assert_eq!(2, records.len());
+            for j in 0..2 as i64 {
+                let record = &records[j];
+                assert_eq!((i + j) * 10, record.base_offset());
+                assert_eq!(
+                    record.payload(),
+                    Bytes::from(format!("Hello World {i:0>8}!", i = i + j))
+                );
+            }
+            info!("Fetch [{start}, {end}) result: {:#?}", records);
+        }
 
-        // reopen the same stream
+        info!("Step4: reopen stream");
+        drop(stream);
+        // await stream close
+        sleep(Duration::from_secs(1)).await;
+        let stream = frontend.open(stream_id, 0).await?;
+
+        info!("Step5: append more 10 record batch");
+        for i in 0..10 {
+            let offset = i + 10;
+            let payload = Bytes::from(format!("Hello World {offset:0>8}!"));
+            let record_batch = RecordBatch::new_builder()
+                .with_stream_id(stream_id as i64)
+                .with_range_index(0)
+                .with_base_offset(0)
+                .with_last_offset_delta(10)
+                .with_payload(payload)
+                .build()
+                .unwrap();
+            let flat_record_batch: FlatRecordBatch = Into::into(record_batch);
+            let (flat_record_batch_bytes, _) = flat_record_batch.encode();
+            let append_result = stream
+                .append(vec_bytes_to_bytes(flat_record_batch_bytes))
+                .await?;
+            assert_eq!(offset * 10, append_result.base_offset as i32);
+            info!("Append result: {:#?}", append_result);
+        }
+
+        info!("Step6: read 20 record batch one by one");
+        for i in 0..20 {
+            let start = i;
+            let end = i + 10;
+            let mut bytes = vec_bytes_to_bytes(stream.read(start, end, i32::MAX).await?);
+            let records = decode_flat_record_batch(&mut bytes)?;
+            assert_eq!(1, records.len());
+            let record = records.iter().next().unwrap();
+            assert_eq!(i * 10, record.base_offset());
+            assert_eq!(
+                record.payload(),
+                Bytes::from(format!("Hello World {i:0>8}!", i = i))
+            );
+            info!("Fetch [{start}, {end}) result: {:#?}", record);
+        }
+
         Ok(())
     })
 }
