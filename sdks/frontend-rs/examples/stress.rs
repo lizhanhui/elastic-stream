3d6af2d48335be9cef894e7e7ca2929f51fdc16e
Ning Yu <chillax65536@gmail.com>
2023-06-08 16:15:42 +0800

    Append by rate
    Signed-off-by: Ning Yu <chillax65536@gmail.com>

--- sdks/frontend-rs/examples/stress.rs
+++ sdks/frontend-rs/examples/stress.rs
@@ -1,26 +1,35 @@
+use std::sync::Arc;
+
 use bytes::{Bytes, BytesMut};
 use frontend::{Frontend, StreamOptions};
 use log::info;
 use model::{record::flat_record::FlatRecordBatch, RecordBatch};
 use rand::RngCore;
-use tokio::time::Duration;
+use tokio::time;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const PM_ADDR: &str = "192.168.123.128:12378";
+    const REPLICA_CNT: u8 = 1;
+    const ACK_CNT: u8 = 1;
+
+    const BATCH_SIZE: usize = 1 * 1024 * 1024;
+    const RATE: u64 = 100;
+
     frontend::init_log();
     tokio_uring::start(async {
-        let frontend = Frontend::new("127.0.0.1:12378")?;
+        let frontend = Frontend::new(PM_ADDR)?;
         let stream_id = frontend
             .create(StreamOptions {
-                replica: 1,
-                ack: 1,
-                retention: Duration::from_secs(3600),
+                replica: REPLICA_CNT,
+                ack: ACK_CNT,
+                retention: time::Duration::from_secs(3600),
             })
             .await?;
         info!("Created stream with id: {}", stream_id);
         let stream = frontend.open(stream_id, 0).await?;
 
         // append 10-record batch
-        let mut payload = [0u8; 1 * 1024 * 1024];
+        let mut payload = [0u8; BATCH_SIZE];
         rand::thread_rng().fill_bytes(&mut payload);
         let record_batch = RecordBatch::new_builder()
             .with_stream_id(stream_id as i64)
@@ -34,19 +43,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let (flat_record_batch_vec_bytes, _) = flat_record_batch.encode();
         let flat_record_batch_bytes = vec_bytes_to_bytes(flat_record_batch_vec_bytes);
 
-        for _ in 0..10 {
-            append(&stream, flat_record_batch_bytes.clone()).await?;
+        let stream = Arc::new(stream);
+        let mut i: u32 = 0;
+        let start = time::Instant::now();
+        let interval_ns = 1_000_000_000 / RATE;
+        loop {
+            let b = flat_record_batch_bytes.clone();
+            let s = stream.clone();
+            i = i + 1;
+            tokio_uring::spawn(async move {
+                append(&s, b, i).await.unwrap();
+            });
+            time::sleep_until(start + time::Duration::from_nanos(interval_ns) * i).await;
         }
-
-        Ok(())
     })
 }
 
-async fn append(stream: &frontend::Stream, bytes: Bytes) -> Result<(), Box<dyn std::error::Error>> {
-    let start = std::time::Instant::now();
+async fn append(
+    stream: &frontend::Stream,
+    bytes: Bytes,
+    index: u32,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let start = time::Instant::now();
+    info!("[{}] Append {} bytes", index, bytes.len());
     let append_result = stream.append(bytes).await?;
     info!(
-        "Append result: {:?}, used {:?}",
+        "[{}] Append result: {:?}, used {:?}",
+        index,
         append_result,
         start.elapsed()
     );
