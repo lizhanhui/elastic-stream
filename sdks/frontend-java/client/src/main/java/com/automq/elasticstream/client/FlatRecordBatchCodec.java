bb05781adca5f3e7e9b4d99f84d6c9bfcd429872
Li Zhanhui <lizhanhui@gmail.com>
2023-06-09 03:08:35 +0000

    fix #501 Use flatbuffers recommended way of encoding
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- sdks/frontend-java/client/src/main/java/com/automq/elasticstream/client/FlatRecordBatchCodec.java
+++ sdks/frontend-java/client/src/main/java/com/automq/elasticstream/client/FlatRecordBatchCodec.java
@@ -6,6 +6,7 @@ import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.buffer.Unpooled;
 import com.automq.elasticstream.client.api.RecordBatch;
 import com.automq.elasticstream.client.api.RecordBatchWithContext;
+import com.automq.elasticstream.client.flatc.records.KeyValue;
 import com.automq.elasticstream.client.flatc.records.KeyValueT;
 import com.automq.elasticstream.client.flatc.records.RecordBatchMeta;
 import com.automq.elasticstream.client.flatc.records.RecordBatchMetaT;
@@ -13,6 +14,7 @@ import com.automq.elasticstream.client.flatc.records.RecordBatchMetaT;
 import java.nio.ByteBuffer;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * RecordBatch =>
@@ -67,6 +69,63 @@ public class FlatRecordBatchCodec {
         return buf;
     }
 
+    /**
+     * Encode RecordBatch to storage format record.
+     *
+     * @param recordBatch {@link RecordBatch}
+     * @return storage format record bytes.
+     */
+    public static ByteBuf encode2(long streamId, RecordBatch recordBatch) {
+
+        int totalLength = 0;
+
+        totalLength += 1; // magic
+
+        FlatBufferBuilder metaBuilder = new FlatBufferBuilder();
+
+        Integer propsVector = null;
+        int propsSize = recordBatch.properties().size();
+        if (propsSize > 0) {
+            int[] kvs = new int[propsSize];
+            int index = 0;
+            for (Map.Entry<String, String> kv : recordBatch.properties().entrySet()) {
+                int k = metaBuilder.createString(kv.getKey());
+                int v = metaBuilder.createString(kv.getValue());
+                int kv_ptr = KeyValue.createKeyValue(metaBuilder, k, v);
+                kvs[index++] = kv_ptr;
+            }
+            propsVector = RecordBatchMeta.createPropertiesVector(metaBuilder, kvs);
+        }
+
+        // encode RecordBatchMeta
+        RecordBatchMeta.startRecordBatchMeta(metaBuilder);
+        RecordBatchMeta.addStreamId(metaBuilder, streamId);
+        RecordBatchMeta.addLastOffsetDelta(metaBuilder, recordBatch.count());
+        RecordBatchMeta.addBaseTimestamp(metaBuilder, recordBatch.baseTimestamp());
+        if (null != propsVector) {
+            RecordBatchMeta.addProperties(metaBuilder, propsVector);
+        }
+        int ptr = RecordBatchMeta.endRecordBatchMeta(metaBuilder);
+        metaBuilder.finish(ptr, null);
+
+        // The data in this ByteBuffer does NOT start at 0, but at buf.position().
+        // The number of bytes is buf.remaining().
+        ByteBuffer metaBuf = metaBuilder.dataBuffer();
+
+        totalLength += 4; // meta length
+        totalLength += metaBuf.remaining(); // RecordBatchMeta
+
+        totalLength += 4; // payload length
+        totalLength += recordBatch.rawPayload().remaining(); // payload
+
+        ByteBuf buf = ALLOCATOR.directBuffer(totalLength);
+        buf.writeByte(MAGIC_V0); // magic
+        buf.writeInt(metaBuf.remaining()); // meta length
+        buf.writeBytes(metaBuf); // RecordBatchMeta
+        buf.writeInt(recordBatch.rawPayload().remaining()); // payload length
+        buf.writeBytes(recordBatch.rawPayload()); // payload
+        return buf;
+    }
 
     /**
      * Decode storage format record to RecordBatchWithContext list.
