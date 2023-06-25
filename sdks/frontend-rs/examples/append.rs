b8312af69ae56826578ad453aaf207eabd75c875
Zhanhui Li <lizhanhui@gmail.com>
2023-06-25 14:59:54 +0800

    feat(example): Polish append work load generator (#589)
    * Add sample usage of local_sync::Semaphore
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>
    
    * Optimize append
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>
    
    ---------
    
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- sdks/frontend-rs/examples/append.rs
+++ sdks/frontend-rs/examples/append.rs
@@ -1,7 +1,7 @@
 use bytes::{Bytes, BytesMut};
 use clap::{arg, Parser};
 use frontend::{Frontend, StreamOptions};
-use futures::future;
+use local_sync::semaphore::Semaphore;
 use log::info;
 use model::{record::flat_record::FlatRecordBatch, RecordBatch};
 use std::{error::Error, rc::Rc};
@@ -16,7 +16,7 @@ struct Args {
 
     /// Number of iterations to append to each stream
     #[arg(short, long, default_value_t = 128)]
-    iteration: usize,
+    time: usize,
 
     #[arg(short, long, default_value_t = 16)]
     batch_size: usize,
@@ -45,29 +45,41 @@ fn main() -> Result<(), Box<dyn Error + 'static>> {
                         .await
                         .unwrap();
                     info!("Created stream with id: {}", stream_id);
-                    let stream = frontend.open(stream_id, 0).await.unwrap();
+                    let stream = Rc::new(frontend.open(stream_id, 0).await.unwrap());
                     let mut payload = BytesMut::with_capacity(args.payload_size);
                     payload.resize(args.payload_size, b'x');
                     let payload = payload.freeze();
                     debug_assert_eq!(args.payload_size, payload.len());
-                    for _ in 0..args.iteration {
-                        let futures = (0..args.batch_size)
-                            .map(|_| {
-                                let record_batch = RecordBatch::new_builder()
-                                    .with_stream_id(stream_id as i64)
-                                    .with_range_index(0)
-                                    .with_base_offset(0)
-                                    .with_last_offset_delta(10)
-                                    .with_payload(payload.clone())
-                                    .build()
-                                    .unwrap();
-                                let flat_record_batch: FlatRecordBatch = Into::into(record_batch);
-                                let (flat_record_batch_bytes, _) = flat_record_batch.encode();
-                                stream.append(vec_bytes_to_bytes(flat_record_batch_bytes))
-                            })
-                            .collect::<Vec<_>>();
-                        future::join_all(futures).await;
-                        info!("Appended {} record batches", args.batch_size);
+                    let semaphore = Rc::new(Semaphore::new(args.batch_size));
+                    let instant = minstant::Instant::now();
+                    let record_batch = RecordBatch::new_builder()
+                        .with_stream_id(stream_id as i64)
+                        .with_range_index(0)
+                        .with_base_offset(0)
+                        .with_last_offset_delta(10)
+                        .with_payload(payload.clone())
+                        .build()
+                        .unwrap();
+                    let flat_record_batch: FlatRecordBatch = Into::into(record_batch);
+                    let (flat_record_batch_bytes, _) = flat_record_batch.encode();
+                    let buf = vec_bytes_to_bytes(flat_record_batch_bytes);
+                    loop {
+                        if instant.elapsed().as_secs() as usize > args.time {
+                            break;
+                        }
+                        match Rc::clone(&semaphore).acquire_owned().await {
+                            Ok(permit) => {
+                                let stream_ = Rc::clone(&stream);
+                                let buf_ = buf.clone();
+                                tokio_uring::spawn(async move {
+                                    if let Err(_) = stream_.append(buf_).await {
+                                        eprintln!("Failed to append");
+                                    }
+                                    drop(permit);
+                                });
+                            }
+                            Err(_e) => break,
+                        }
                     }
                 })
             })
