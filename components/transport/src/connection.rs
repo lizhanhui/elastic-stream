1f4a1021004aaa7ad6322301eb6534e5d0e0937b
Li Zhanhui <lizhanhui@gmail.com>
2023-08-10 13:02:53 +0000

    WIP
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- components/transport/src/connection.rs
+++ components/transport/src/connection.rs
@@ -5,160 +5,28 @@ use codec::error::FrameError;
 use codec::frame::Frame;
 use local_sync::{mpsc, oneshot};
 use log::{error, info, trace, warn};
-use std::cell::UnsafeCell;
+use monoio::io::{AsyncReadRent, AsyncWriteRent, AsyncWriteRentExt};
 use std::io::Cursor;
-use std::net::Shutdown;
-use std::rc::Rc;
-use tokio_uring::net::TcpStream;
 
 const BUFFER_SIZE: usize = 4 * 1024;
 
-pub struct Connection {
-    /// Underlying TCP stream.
-    stream: Rc<TcpStream>,
-
-    /// Read buffer for this connection.
-    buffer: UnsafeCell<BytesMut>,
-
+pub struct ChannelReader<S> {
+    stream: S,
     peer_address: String,
-
-    /// Write buffer for this connection.
-    ///
-    /// Writes from concurrent coroutines are serialized by this MPSC channel.
-    tx: mpsc::unbounded::Tx<WriteTask>,
+    /// Read buffer for this connection.
+    buffer: BytesMut,
 }
 
-impl Connection {
-    pub fn new(stream: TcpStream, peer_address: &str) -> Self {
-        let stream = Rc::new(stream);
-        let (tx, mut rx) = mpsc::unbounded::channel::<WriteTask>();
-
-        let write_stream = Rc::clone(&stream);
-        let target_address = peer_address.to_owned();
-        tokio_uring::spawn(async move {
-            trace!("Start write coroutine loop for {}", &target_address);
-            loop {
-                match rx.recv().await {
-                    Some(task) => {
-                        trace!(
-                            "Write-frame-task[stream-id={}] received, start writing to {}",
-                            task.frame.stream_id,
-                            &target_address
-                        );
-
-                        if let Err(e) =
-                            Self::write(&write_stream, &task.frame, &target_address).await
-                        {
-                            match e {
-                                ConnectionError::EncodeFrame(e) => {
-                                    error!("Failed to encode frame: {}", e.to_string());
-                                    let _ =
-                                        task.observer.send(Err(ConnectionError::EncodeFrame(e)));
-                                }
-
-                                ConnectionError::Network(e) => {
-                                    error!("Failed to write frame to network due to {}, closing underlying TcpStream", e.to_string());
-                                    let _ = write_stream.shutdown(Shutdown::Both);
-                                    break;
-                                }
-                            }
-                        } else {
-                            let _ = task.observer.send(Ok(()));
-                        }
-                    }
-                    None => {
-                        log::info!(
-                            "Connection to {} should be closed, stop write coroutine loop",
-                            &target_address
-                        );
-                        break;
-                    }
-                }
-            }
-        });
-
+impl<S> ChannelReader<S>
+where
+    S: AsyncReadRent,
+{
+    pub fn new(stream: S, peer_address: String) -> Self {
         Self {
             stream,
-            buffer: UnsafeCell::new(BytesMut::with_capacity(BUFFER_SIZE)),
-            peer_address: peer_address.to_owned(),
-            tx,
+            peer_address,
+            buffer: BytesMut::with_capacity(BUFFER_SIZE),
         }
-    }
-
-    async fn write(
-        stream: &Rc<TcpStream>,
-        frame: &Frame,
-        peer_address: &str,
-    ) -> Result<(), ConnectionError> {
-        let mut buffers = frame.encode()?;
-
-        let total = buffers.iter().map(|b| b.len()).sum::<usize>();
-        trace!("Get {} bytes to write to: {}", total, peer_address);
-        let mut remaining = total;
-        loop {
-            let (res, _buffers) = stream.writev(buffers).await;
-            buffers = _buffers;
-            let mut n = res?;
-            debug_assert!(n <= remaining, "Data written to socket buffer should be less than or equal to remaining bytes to write");
-            if n == remaining {
-                if remaining == total {
-                    // First time to write
-                    trace!("Wrote {}/{} bytes to {}", n, total, peer_address);
-                } else {
-                    // Last time of writing: the remaining are all written.
-                    remaining -= n;
-                    trace!(
-                        "Wrote {}/{} bytes to {}. Overall, {}/{} is written.",
-                        n,
-                        total,
-                        peer_address,
-                        total - remaining,
-                        total
-                    );
-                }
-                break;
-            } else {
-                remaining -= n;
-                trace!(
-                    "Wrote {} bytes to {}. Overall, {}/{} is written",
-                    n,
-                    peer_address,
-                    total - remaining,
-                    total,
-                );
-                // Drain/advance buffers that are already written.
-                buffers
-                    .extract_if(|buffer| {
-                        if buffer.len() <= n {
-                            n -= buffer.len();
-                            // Remove it
-                            true
-                        } else {
-                            if n > 0 {
-                                buffer.advance(n);
-                                n = 0;
-                            }
-                            // Keep the buffer slice
-                            false
-                        }
-                    })
-                    .count();
-            }
-        }
-        Ok(())
-    }
-
-    pub fn peer_address(&self) -> &str {
-        &self.peer_address
-    }
-
-    #[allow(clippy::mut_from_ref)]
-    pub fn buf_mut(&self) -> &mut BytesMut {
-        unsafe { &mut *self.buffer.get() }
-    }
-
-    pub fn buf(&self) -> &BytesMut {
-        unsafe { &*self.buffer.get() }
     }
 
     /// Read a single `Frame` value from the underlying stream.
@@ -172,7 +40,7 @@ impl Connection {
     /// On success, the received frame is returned. If the `TcpStream`
     /// is closed in a way that doesn't break a frame in half, it returns
     /// `None`. Otherwise, an error is returned.
-    pub async fn read_frame(&self) -> Result<Option<Frame>, FrameError> {
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>, FrameError> {
         loop {
             // Attempt to parse a frame from the buffered data. If enough data
             // has been buffered, the frame is returned.
@@ -181,9 +49,8 @@ impl Connection {
             }
 
             // Try to allocate more memory from allocator
-            let buffer = self.buf_mut();
-            if buffer.spare_capacity_mut().len() < BUFFER_SIZE {
-                buffer.reserve(BUFFER_SIZE);
+            if self.buffer.spare_capacity_mut().len() < BUFFER_SIZE {
+                self.buffer.reserve(BUFFER_SIZE);
             }
 
             // There is not enough buffered data to read a frame. Attempt to
@@ -191,10 +58,10 @@ impl Connection {
             //
             // On success, the number of bytes is returned. `0` indicates "end
             // of stream".
-            let len = buffer.len();
-            let buf = buffer.split_off(len);
+            let len = self.buffer.len();
+            let buf = self.buffer.split_off(len);
             let (res, buf) = self.stream.read(buf).await;
-            buffer.unsplit(buf);
+            self.buffer.unsplit(buf);
 
             let read = match res {
                 Ok(n) => {
@@ -212,10 +79,10 @@ impl Connection {
                 // shutdown, there should be no data in the read buffer. If
                 // there is, this means that the peer closed the socket while
                 // sending a frame.
-                if buffer.is_empty() {
+                if self.buffer.is_empty() {
                     return Ok(None);
                 } else {
-                    warn!("Discarded {} bytes", buffer.len());
+                    warn!("Discarded {} bytes", self.buffer.len());
                     return Err(FrameError::ConnectionReset);
                 }
             }
@@ -226,15 +93,14 @@ impl Connection {
     /// data, the frame is returned and the data removed from the buffer. If not
     /// enough data has been buffered yet, `Ok(None)` is returned. If the
     /// buffered data does not represent a valid frame, `Err` is returned.
-    fn parse_frame(&self) -> Result<Option<Frame>, FrameError> {
+    fn parse_frame(&mut self) -> Result<Option<Frame>, FrameError> {
         use FrameError::Incomplete;
 
         // Cursor is used to track the "current" location in the
         // buffer. Cursor also implements `Buf` from the `bytes` crate
         // which provides a number of helpful utilities for working
         // with bytes.
-        let buffer = self.buf();
-        let mut buf = Cursor::new(&buffer[..]);
+        let mut buf = Cursor::new(&self.buffer[..]);
 
         // The first step is to check if enough data has been buffered to parse
         // a single frame. This step is usually much faster than doing a full
@@ -268,7 +134,7 @@ impl Connection {
                 // up to `len` is discarded. The details of how this works is
                 // left to `BytesMut`. This is often done by moving an internal
                 // cursor, but it may be done by reallocating and copying data.
-                self.buf_mut().advance(len);
+                self.buffer.advance(len);
 
                 // Return the parsed frame to the caller.
                 Ok(Some(frame))
@@ -288,6 +154,89 @@ impl Connection {
             // in the connection being closed.
             Err(e) => Err(e),
         }
+    }
+}
+
+pub struct ChannelWriter {
+    peer_address: String,
+    tx: mpsc::unbounded::Tx<WriteTask>,
+}
+
+impl ChannelWriter {
+    pub fn new<S>(mut stream: S, peer_address: String) -> Self
+    where
+        S: AsyncWriteRent + 'static,
+    {
+        let (tx, mut rx) = mpsc::unbounded::channel::<WriteTask>();
+        let target_address = peer_address.clone();
+        monoio::spawn(async move {
+            trace!("Start write coroutine loop for {}", &target_address);
+            loop {
+                match rx.recv().await {
+                    Some(task) => {
+                        trace!(
+                            "Write-frame-task[stream-id={}] received, start writing to {}",
+                            task.frame.stream_id,
+                            &target_address
+                        );
+
+                        if let Err(e) = Self::write(&mut stream, &task.frame, &target_address).await
+                        {
+                            match e {
+                                ConnectionError::EncodeFrame(e) => {
+                                    error!("Failed to encode frame: {}", e.to_string());
+                                    let _ =
+                                        task.observer.send(Err(ConnectionError::EncodeFrame(e)));
+                                }
+
+                                ConnectionError::Network(e) => {
+                                    error!("Failed to write frame to network due to {}, closing underlying TcpStream", e.to_string());
+                                    let _ = stream.shutdown().await;
+                                    break;
+                                }
+                            }
+                        } else {
+                            let _ = task.observer.send(Ok(()));
+                        }
+                    }
+                    None => {
+                        log::info!(
+                            "Connection to {} should be closed, stop write coroutine loop",
+                            &target_address
+                        );
+                        break;
+                    }
+                }
+            }
+        });
+
+        Self { peer_address, tx }
+    }
+
+    async fn write<S>(
+        stream: &mut S,
+        frame: &Frame,
+        peer_address: &str,
+    ) -> Result<(), ConnectionError>
+    where
+        S: AsyncWriteRent,
+    {
+        let buffers = frame.encode()?;
+        let io_vec = buffers
+            .iter()
+            .map(|buf| {
+                let ptr = buf.as_ptr();
+                let len = buf.len();
+                libc::iovec {
+                    iov_base: ptr as *mut libc::c_void,
+                    iov_len: len as _,
+                }
+            })
+            .collect::<Vec<_>>();
+        let (res, _b) = stream.write_vectored_all(io_vec).await;
+        let written = res.map_err(ConnectionError::Network)?;
+        trace!("Wrote {written} bytes to {peer_address}");
+        Ok(())
     }
 
     pub async fn write_frame(&self, frame: Frame) -> Result<(), ConnectionError> {
@@ -311,10 +260,6 @@ impl Connection {
             ))
         })?
     }
-
-    pub fn close(&self) -> std::io::Result<()> {
-        self.stream.shutdown(std::net::Shutdown::Both)
-    }
 }
 
 #[cfg(test)]
@@ -322,8 +267,11 @@ mod tests {
     use std::{error::Error, thread::JoinHandle};
 
     use bytes::{Buf, BufMut, BytesMut};
+    use monoio::{
+        io::{AsyncReadRent, Splitable},
+        net::{TcpListener, TcpStream},
+    };
     use protocol::rpc::header::OperationCode;
-    use tokio::{io::AsyncReadExt, net::TcpListener};
 
     #[test]
     fn test_bytes_concept() {
@@ -361,24 +309,26 @@ mod tests {
     }
 
     fn run_server(
-        counter: tokio::sync::oneshot::Sender<usize>,
-        port: tokio::sync::oneshot::Sender<u16>,
+        counter: flume::Sender<usize>,
+        port: flume::Sender<u16>,
     ) -> Result<JoinHandle<()>, Box<dyn Error>> {
         let handle = std::thread::Builder::new()
             .name("Test-Server".to_owned())
             .spawn(move || {
-                let rt = tokio::runtime::Builder::new_current_thread()
+                let rt = monoio::RuntimeBuilder::<monoio::FusionDriver>::new()
                     .enable_all()
                     .build()
                     .unwrap();
+
                 rt.block_on(async move {
-                    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+                    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
                     let local_addr = listener.local_addr().unwrap();
                     port.send(local_addr.port()).unwrap();
                     let (mut stream, _addr) = listener.accept().await.unwrap();
                     let mut read = 0;
                     let mut buf = BytesMut::with_capacity(1024);
-                    while let Ok(n) = stream.read_buf(&mut buf).await {
+                    while let (res, buf_) = stream.read(buf).await {
+                        let n = res.unwrap();
                         if 0 == n {
                             break;
                         }
@@ -394,21 +344,23 @@ mod tests {
     #[test]
     fn test_write_frame() -> Result<(), Box<dyn Error>> {
         ulog::try_init_log();
-        let (counter_tx, counter_rx) = tokio::sync::oneshot::channel();
-        let (port_tx, port_rx) = tokio::sync::oneshot::channel();
+        let (counter_tx, counter_rx) = flume::bounded(1);
+        let (port_tx, port_rx) = flume::bounded(1);
 
         let handle = run_server(counter_tx, port_tx).unwrap();
 
-        let port = port_rx.blocking_recv()?;
+        let port = port_rx.recv()?;
+        let rt = monoio::RuntimeBuilder::<monoio::FusionDriver>::new()
+            .enable_all()
+            .build()?;
 
-        tokio_uring::start(async {
+        rt.block_on(async move {
             let address = format!("127.0.0.1:{}", port);
             {
-                let tcp_stream = tokio_uring::net::TcpStream::connect(address.parse().unwrap())
-                    .await
-                    .unwrap();
+                let tcp_stream = TcpStream::connect(&address).await.unwrap();
                 tcp_stream.set_nodelay(true).unwrap();
-                let connection = super::Connection::new(tcp_stream, &address);
+                let (read_half, write_half) = tcp_stream.into_split();
+                let channel_writer = super::ChannelWriter::new(write_half, address.clone());
                 let mut frame = codec::frame::Frame::new(OperationCode::ALLOCATE_ID);
                 let mut payload = vec![];
                 (0..8).for_each(|_| {
@@ -417,11 +369,11 @@ mod tests {
                     payload.push(buf.freeze());
                 });
                 frame.payload = Some(payload);
-                connection.write_frame(frame).await.unwrap();
-                connection.close().unwrap();
+                channel_writer.write_frame(frame).await.unwrap();
+                drop(channel_writer);
             }
 
-            let total = counter_rx.await.unwrap();
+            let total = counter_rx.recv_async().await.unwrap();
             assert_eq!(total, 1024 * 1024 * 8 + 20);
         });
 
